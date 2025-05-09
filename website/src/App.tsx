e13540894ec7ed4986dd591e0abecba07aded7c1
nilsreichardt <me@nils.re>
2025-05-09 18:44:57 +0200

    Add "/submission" disclaimer

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Guidebook from "./pages/Guidebook";
+import Submission from "./pages/Submission";
 
 const queryClient = new QueryClient();
 
@@ -19,6 +20,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
           <Route path="/guidebook" element={<Guidebook />} />
+          <Route path="/submission" element={<Submission />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
